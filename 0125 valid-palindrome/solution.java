f556652a512e859789381021fb5a7f6399b9155b
Feroz <mdferozahmed27156@gmail.com>
2026-01-06 08:29:36 +0000

    Solved Valid Palindrome

--- 0125 valid-palindrome/solution.java
+++ 0125 valid-palindrome/solution.java
@@ -1,16 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-        int left=0;
-        int right = s.length()-1;
-        while(left<right){
-            if(s.charAt(left)!=s.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-       
+        String c = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String r = new StringBuilder(c).reverse().toString();
+        return c.equals(r);
     }
 }
