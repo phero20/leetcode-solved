13b13b0d5e7c9d4c8e74695440326b2afa270df1
Feroz <mdferozahmed27156@gmail.com>
2026-01-12 03:40:50 +0000

    Solved Reverse String

---
+++ 0344 reverse-string/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int length = s.length-1;
+        for(int i=0;i<s.length/2;i++){
+            char temp = s[length-i];
+            s[length-i]=s[i];
+            s[i]=temp;
+        }
+    }
+}
