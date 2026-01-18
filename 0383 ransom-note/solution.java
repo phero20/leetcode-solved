0dc284755ffab2df5edb96e0c186ace4c088d958
Feroz <mdferozahmed27156@gmail.com>
2026-01-18 05:32:45 +0000

    Solved Ransom Note

--- 0383 ransom-note/solution.java
+++ 0383 ransom-note/solution.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-       int[] arr=new int[26];
-       char[] m = magazine.toCharArray();
-       char[] r=ransomNote.toCharArray();
-       for(char c:m) arr[c-'a']++;
-       for(char c:r){
-        if(--arr[c-'a']<0) return false;
+       int arr[] = new int[26];
+       char[] cr=magazine.toCharArray();
+       char[] rr = ransomNote.toCharArray();
+       for(char c:cr) arr[c-'a']++;
+       for(char c:rr) {
+         arr[c-'a']--;
+         if(arr[c-'a']<0) return false;
        }
        return true;
     }
