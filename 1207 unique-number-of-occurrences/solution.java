1cc81f78439375f012b74481dd9aa383a72c32f4
Feroz <mdferozahmed27156@gmail.com>
2026-01-19 06:43:33 +0000

    Solved Unique Number of Occurrences

--- 1207 unique-number-of-occurrences/solution.java
+++ 1207 unique-number-of-occurrences/solution.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        Set<Integer> s = new HashSet<>();
-        for(int n:arr) 
-        if(map.containsKey(n)) map.put(n, map.get(n) + 1);
-        else map.put(n,1);
-        for(int v:map.values())
-            if(s.add(v)==false) return false;
-        return true;
+      HashMap<Integer,Integer> map = new HashMap<>();
+      for(int i:arr) map.put(i,map.getOrDefault(i,0)+1);
+      Set<Integer> s=new HashSet<>();
+      for(int i:map.values()){
+        if(!s.add(i)) return false;
+      }
+      return true;
     }
 }
