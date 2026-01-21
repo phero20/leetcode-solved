6e165cb755f5d18528a9d1e013e2ce26352df12c
Feroz <mdferozahmed27156@gmail.com>
2026-01-21 06:09:56 +0000

    Solved Find Lucky Integer in an Array

--- 1394 find-lucky-integer-in-an-array/solution.java
+++ 1394 find-lucky-integer-in-an-array/solution.java
@@ -2,9 +2,7 @@ class Solution {
     public int findLucky(int[] arr) {
      int[] a=new int[501];
      for(int i:arr) a[i]++;
-     for(int i=500;i>0;i--){
-        if(a[i]==i) return i;
-     }
-       return -1;
-    }
-}
+     for(int i=500;i>0;i--)
+     if(a[i]==i) return i;
+     return -1;
+}}
