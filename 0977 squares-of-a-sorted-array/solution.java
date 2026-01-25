607e2f4ae4040854ddff598abb9d11e66d446cb4
Feroz <mdferozahmed27156@gmail.com>
2026-01-25 03:36:08 +0000

    Solved Squares of a Sorted Array

--- 0977 squares-of-a-sorted-array/solution.java
+++ 0977 squares-of-a-sorted-array/solution.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-       int i=0;
+        int i=0;
         int j=nums.length-1;
         int arr[]=new int[nums.length];
         for(int k=nums.length-1;k>=0;k--){
             int s1=nums[i]*nums[i];
             int s2=nums[j]*nums[j];
-            if(s2>s1){
-                arr[k]=s2;
-                j--;
-            }
-            else {
+            if(s1>s2){
                 arr[k]=s1;
                 i++;
             }
+            else{
+                arr[k]=s2;
+                j--;
+            }
         }
-         return arr;
+        return arr;
+       
     }
 }
