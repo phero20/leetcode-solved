b157b561467d68a197cb10fb8e4466752122b463
Feroz <mdferozahmed27156@gmail.com>
2026-02-01 11:51:13 +0000

    Solved Tree Node

---
+++ 0608 tree-node/solution.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+
+
+
+
+
+select t1.id,
+case when
+    t1.p_id is null then 'Root'
+    when not exists(
+        select 1 from Tree t2 where t2.p_id=t1.id
+    )
+    then 'Leaf'
+    else 'Inner'
+END AS type
+FROM Tree t1;
+
+
+
+
+
+
