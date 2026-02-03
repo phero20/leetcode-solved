bb40444e30ceb7bbb7db6e71581f2995402b99ae
Feroz <mdferozahmed27156@gmail.com>
2026-02-03 12:12:09 +0000

    Solved Customers Who Never Order

--- 0183 customers-who-never-order/solution.sql
+++ 0183 customers-who-never-order/solution.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
 
 
-select name as Customers from Customers where id not in (select customerId from Orders);
+
+select name as Customers from Customers where id not in
+(select customerId from Orders);
