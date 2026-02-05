d7751ff9710f53513493446b9338953d6b571295
Feroz <mdferozahmed27156@gmail.com>
2026-02-05 14:23:57 +0000

    Solved Group Sold Products By The Date

---
+++ 1484 group-sold-products-by-the-date/solution.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+
+
+
+
+
+
+
+
+
+
+SELECT 
+    sell_date,
+    COUNT(distinct product) AS num_sold,
+    GROUP_CONCAT(distinct product ORDER BY product SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date;
