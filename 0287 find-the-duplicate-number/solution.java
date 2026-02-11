030346d200ce59ed3a49bad746a89ad941955c29
Feroz <mdferozahmed27156@gmail.com>
2026-02-11 04:27:54 +0000

    Solved Find the Duplicate Number

--- 0287 find-the-duplicate-number/solution.java
+++ 0287 find-the-duplicate-number/solution.java
@@ -1,10 +1,11 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-      boolean b[] = new boolean[nums.length];
-      for(int n:nums) { 
-        if(b[n]) return n;
-        else b[n]=true;
-         }
-      return 0;
+    boolean set[] = new boolean[nums.length];
+     for(int i:nums) {
+         if(set[i-1]) return i;
+         set[i-1]=true;
+     }
+       
+            return 0;
     }
 }
