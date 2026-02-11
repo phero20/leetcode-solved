6a928448ccd50045981a526dcdb206be371afb84
Feroz <mdferozahmed27156@gmail.com>
2026-02-11 03:41:22 +0000

    Solved Missing Number

--- 0268 missing-number/solution.java
+++ 0268 missing-number/solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-      int total = (n * (n+1)) /2;
-      int sum=0;
-      for(int num : nums){
-        sum+=num;
-      }
-      return total-sum;
-}}
+        int l=nums.length;
+        int cal=(l*(l+1))/2;
+        int sum=0;
+        for(int i:nums) sum+=i;
+        return cal-sum;
+    }
+}
