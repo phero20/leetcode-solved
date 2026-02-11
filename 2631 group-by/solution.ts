9a7a5435db5cb81838b854f5cadc0d67a5ecf81c
Feroz <mdferozahmed27156@gmail.com>
2026-02-11 04:50:33 +0000

    Solved Group By

---
+++ 2631 group-by/solution.ts
@@ -0,0 +1,18 @@
+interface Array<T> {
+    groupBy(fn: (item: T) => string): Record<string, T[]>
+}
+
+
+Array.prototype.groupBy = function(fn) {
+    let res={}
+    for(let i=0;i<this.length;i++){
+        const key = fn(this[i])
+        if(!res[key]) res[key]=[]
+        res[key].push(this[i])
+    }
+    return res
+}
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
