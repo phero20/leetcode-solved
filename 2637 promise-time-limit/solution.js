d5d73bc213d07ac5eedbb1c940b7d6a2fadc9fa5
Feroz <mdferozahmed27156@gmail.com>
2026-02-11 04:58:53 +0000

    Solved Promise Time Limit

---
+++ 2637 promise-time-limit/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return new Promise((resolve, reject) => {
+            // Run the function
+            const timer = setTimeout(() => reject("Time Limit Exceeded"), t);
+
+            Promise.resolve(fn(...args))
+                .then((res) => {
+                    clearTimeout(timer);
+                    resolve(res);
+                })
+                .catch((err) => {
+                    clearTimeout(timer);
+                    reject(err);
+                });
+        });
+    };
+};
