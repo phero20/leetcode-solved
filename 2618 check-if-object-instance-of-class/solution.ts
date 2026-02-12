f83b7c151053569e7b5fbd419fe2a8737380e344
Feroz <mdferozahmed27156@gmail.com>
2026-02-12 13:21:31 +0000

    Solved Check if Object Instance of Class

---
+++ 2618 check-if-object-instance-of-class/solution.ts
@@ -0,0 +1,13 @@
+function checkIfInstanceOf(obj: any, classFunction: any): boolean {
+    if(obj===null || obj===undefined || typeof classFunction !== 'function') return false;
+    let proto = Object.getPrototypeOf(obj);
+    while(proto!==null) {
+        if(proto===classFunction.prototype) return true;
+        proto = Object.getPrototypeOf(proto);
+    }
+    return false
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
