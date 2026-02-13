c72e57214c663d2c874d1a941e2041b9802ac7a9
Feroz <mdferozahmed27156@gmail.com>
2026-02-13 07:36:43 +0000

    Solved Product of Array Except Self

---
+++ 0238 product-of-array-except-self/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int prefix=1,suffix=1;
+        int ans[]=new int[n];
+        for(int i=0;i<n;i++) {
+            ans[i]=prefix;
+            prefix*=nums[i];
+        }
+        for(int i=n-1;i>=0;i--) {
+            ans[i]*=suffix;
+            suffix*=nums[i];
+        }
+        return ans;
+    }
+}
