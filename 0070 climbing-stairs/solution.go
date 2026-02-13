02a2d34fde100f23b1898b2fae1356a371c7c646
Feroz <mdferozahmed27156@gmail.com>
2026-02-13 06:32:53 +0000

    Solved Climbing Stairs

---
+++ 0070 climbing-stairs/solution.go
@@ -0,0 +1,12 @@
+func climbStairs(n int) int {
+    if n==1 || n==2 {
+        return n
+    }
+    first,second:=1,2
+    for i:=3;i<=n;i++ {
+        third:=first+second
+        first = second
+        second = third
+    }
+    return second
+}
