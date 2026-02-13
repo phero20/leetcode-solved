634271678da3bd1752ae20b84816388e9fe2f532
Feroz <mdferozahmed27156@gmail.com>
2026-02-13 06:26:24 +0000

    Solved First Bad Version

---
+++ 0278 first-bad-version/solution.go
@@ -0,0 +1,25 @@
+/** 
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad 
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+   low,high:=1,n
+   for low<high {
+    if low==high {
+        return low
+    }
+    mid:=low+(high-low)/2
+    if(isBadVersion(mid)) {
+        high=mid
+    } else {
+        low=mid+1
+    }
+   }
+   return low
+}
+
+
