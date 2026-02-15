4d1ccbd2e075fc04c66050f445018f90fad3c431
Feroz <mdferozahmed27156@gmail.com>
2026-02-15 07:11:01 +0000

    Solved Remove Duplicates from Sorted Array II

---
+++ 0080 remove-duplicates-from-sorted-array-ii/solution.go
@@ -0,0 +1,16 @@
+func removeDuplicates(nums []int) int {
+    j,count:=1,1
+    for i:=1;i<len(nums);i++ {
+        if nums[i]==nums[i-1] {
+            count++
+        } else {
+            count=1
+        }
+
+        if count <=2 {
+            nums[j]=nums[i]
+            j++
+        }
+    }
+    return j
+}
