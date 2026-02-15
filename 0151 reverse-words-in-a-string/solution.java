94327f88e88c737d78d015e0e92aa00c3823ee0b
Feroz <mdferozahmed27156@gmail.com>
2026-02-15 10:10:45 +0000

    Solved Reverse Words in a String

---
+++ 0151 reverse-words-in-a-string/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder res = new StringBuilder();
+        int i=s.length()-1;
+        while(i>=0) {
+            while(i>=0 && s.charAt(i)==' ') i--;
+            if (i<0) break;
+            int end=i;
+            while(i>=0 && s.charAt(i)!=' ') i--;
+                String st=s.substring(i+1,end+1);
+                if(res.length()>0) res.append(" ");
+                res.append(st);
+
+        }
+        return res.toString();
+    }
+}
