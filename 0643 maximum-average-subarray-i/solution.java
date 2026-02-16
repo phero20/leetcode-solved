fab3be57e2bca0d4b77f71f2942899a105c1c2f7
Feroz <mdferozahmed27156@gmail.com>
2026-02-16 08:44:56 +0000

    Solved Maximum Average Subarray I

--- 0643 maximum-average-subarray-i/solution.java
+++ 0643 maximum-average-subarray-i/solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double sum=0;
-        for(int i=0;i<k;i++) sum+=nums[i];
-       double maxavg=sum/k;
-       for(int i=k;i<nums.length;i++){
-        sum=sum-nums[i-k]+nums[i];
-        double avg=sum/k;
-        maxavg=Math.max(maxavg,avg);
+       double sum =0;
+       for(int i=0;i<k;i++) sum+=nums[i];
+       double maxAvg = sum/k;
+       for(int i=k;i<nums.length;i++) {
+           sum=(sum-nums[i-k])+nums[i];
+            // sum+=nums[i];
+            // sum-=nums[i-k];
+            maxAvg=Math.max(maxAvg,sum/k);
        }
-        return maxavg;
+       return maxAvg;
     }
 }
