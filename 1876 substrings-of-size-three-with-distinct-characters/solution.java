4087264aae3491e151bf514fa23446a6ac625e75
Feroz <mdferozahmed27156@gmail.com>
2026-02-16 09:27:06 +0000

    Solved Substrings of Size Three with Distinct Characters

---
+++ 1876 substrings-of-size-three-with-distinct-characters/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+        int sum=0;
+        for(int i=0;i<s.length()-2;i++) {
+            char a = s.charAt(i);
+            char b = s.charAt(i+1);
+            char c = s.charAt(i+2);
+            if(a!=b && a!=c && b!=c) sum++;
+        }
+        return sum;
+    }
+}
