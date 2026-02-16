1d691d025db3f7398ad7bce1d179e4f0a2abf790
Feroz <mdferozahmed27156@gmail.com>
2026-02-16 13:52:37 +0000

    Solved Friend Requests II: Who Has the Most Friends

--- 0602 friend-requests-ii-who-has-the-most-friends/solution.sql
+++ 0602 friend-requests-ii-who-has-the-most-friends/solution.sql
@@ -1,13 +1,11 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
 
-
-
-select id as id,count(*) as num from
+select id,count(*) as num from
 (
 select requester_id as id from RequestAccepted union all 
-select accepter_id id from RequestAccepted
-) as allData
+select accepter_id as id from RequestAccepted
+)
 group by id
 order by num desc
 LIMIT 1;
