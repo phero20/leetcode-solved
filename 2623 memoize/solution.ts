e3dbab13b7cecf003e14663a126b07a0fe0d354c
Feroz <mdferozahmed27156@gmail.com>
2026-02-16 10:18:47 +0000

    Solved Memoize

---
+++ 2623 memoize/solution.ts
@@ -0,0 +1,24 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+    let cache = {}
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if(key in cache) return cache[key];
+        let res = fn(...args)
+        cache[key]=res;
+        return res;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
