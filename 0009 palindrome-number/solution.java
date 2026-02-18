c72951ab5b98a299348b977edec9796f39bb5e95
Feroz <mdferozahmed27156@gmail.com>
2026-02-18 15:28:00 +0000

    Solved Palindrome Number

--- 0009 palindrome-number/solution.java
+++ 0009 palindrome-number/solution.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x<0){
-            return false;
+        int n=x;
+        int rev=0;
+        while(n>0) {
+            int dig = n%10;
+            rev=rev*10+dig;
+            n/=10;
         }
-        int reversed=0;
-        int original=x;
-        while (x != 0) {
-            int digit = x % 10;
-            reversed = reversed * 10 + digit;
-            x /= 10;
-        }
-        return reversed==original;
+        return rev==x;
     }
 }
