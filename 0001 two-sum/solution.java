77184a08a24f23dccbb0f32cbe66faecc6d27ad6
Feroz <mdferozahmed27156@gmail.com>
2026-02-18 14:31:32 +0000

    Solved Two Sum

--- 0001 two-sum/solution.java
+++ 0001 two-sum/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++) {
-            int diff = target-nums[i];
-            if(map.containsKey(diff)) {
-                return new int[]{map.get(diff),i};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[] { map.get(diff), i };
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
-        return new int[]{};
+        return new int[] {};
     }
 }
