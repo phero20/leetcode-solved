bf763eada497f499bb869721c0c6587125d7b17f
Feroz <mdferozahmed27156@gmail.com>
2026-02-18 15:44:25 +0000

    Solved Roman to Integer

--- 0013 roman-to-integer/solution.java
+++ 0013 roman-to-integer/solution.java
@@ -1,19 +1,19 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = Map.of(
-            'I', 1, 'V', 5, 'X', 10, 'L', 50,
-            'C', 100, 'D', 500, 'M', 1000
-        );
-        int total = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int value = map.get(s.charAt(i));
-            if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
-                total -= value;
-            } else {
-                total += value;
-            }
+        int arr[]=new int[126];
+        arr['I']=1;
+        arr['V']=5;
+        arr['X']=10;
+        arr['L']=50;
+        arr['C']=100;
+        arr['D']=500;
+        arr['M']=1000;
+        int ans =0;
+        for(int i=0;i<s.length();i++) {
+            int v=arr[s.charAt(i)];
+            if(i+1<s.length() && v<arr[s.charAt(i+1)]) ans-=v;
+            else ans+=v;
         }
-        return total;
+        return ans;
     }
 }
-
