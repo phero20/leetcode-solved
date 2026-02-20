af113146c98da2f261e65c0f1f141713306a6c5f
Feroz <mdferozahmed27156@gmail.com>
2026-02-20 10:24:23 +0000

    Solved Best Time to Buy and Sell Stock

--- 0121 best-time-to-buy-and-sell-stock/solution.java
+++ 0121 best-time-to-buy-and-sell-stock/solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
-    int low= Integer.MAX_VALUE;
-    int gap = 0;
-    for(int i:prices) {
-        if(low>i) low=i;
-        else gap = Math.max(gap,i-low);
+    int gap=0,l=prices[0];  
+    for(int r=1;r<prices.length;r++) {
+        if(prices[r]<l) l=prices[r];
+        gap=Math.max(gap,prices[r]-l);
     }
-    return gap; 
+    return gap;
     }
 }
