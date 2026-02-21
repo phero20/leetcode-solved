9c841d1a38ba51a8f65169feb05010813422c83c
Feroz <mdferozahmed27156@gmail.com>
2026-02-21 03:44:54 +0000

    Solved Best Time to Buy and Sell Stock

--- 0121 best-time-to-buy-and-sell-stock/solution.cs
+++ 0121 best-time-to-buy-and-sell-stock/solution.cs
@@ -1,13 +1,10 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int low=prices[0],gap=0;
-        for(int i=1;i<prices.Length;i++){
-            if(prices[i]<low) {
-                low=prices[i];
-            } else {
-                gap=Math.Max(gap,prices[i]-low);
-            }
-        }
-        return gap;
+       int low=prices[0],gap=0;
+       for(int r=1;r<prices.Length;r++) {
+        if(prices[r]<low) low=prices[r];
+            gap=Math.Max(gap,prices[r]-low);
+       }
+       return gap;
     }
 }
