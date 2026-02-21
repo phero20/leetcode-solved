527c84b9ab3dd8a8c145b21d740a04f7200ce9a4
Feroz <mdferozahmed27156@gmail.com>
2026-02-21 08:08:36 +0000

    Solved Find the Middle Index in Array

---
+++ 1991 find-the-middle-index-in-array/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int sum=0,lsum=0;
+        for(int i:nums) sum+=i;
+        for(int i=0;i<nums.length;i++) {
+            if(lsum==sum-lsum-nums[i]) return i;
+            lsum+=nums[i];
+        }
+        return -1;
+    }
+}
