30e613c4904f0f5b8db948287ccab3fb570951aa
Feroz <mdferozahmed27156@gmail.com>
2026-02-22 09:58:47 +0000

    Solved Find Pivot Index

---
+++ 0724 find-pivot-index/solution.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn pivot_index(nums: Vec<i32>) -> i32 {
+        let mut sum=0;
+        for i in 0..nums.len() {
+            sum+=nums[i];
+        }
+        let mut lsum=0;
+        for i in 0..nums.len() {
+            if lsum==sum-nums[i]-lsum {
+                return i as i32
+            }
+            lsum+=nums[i];
+        }
+        -1
+    }
+}
