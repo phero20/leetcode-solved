b653b9bd7d436d26ea24d1665167f3d35a0a8a5e
Feroz <mdferozahmed27156@gmail.com>
2026-02-22 12:52:32 +0000

    Solved Can Make Arithmetic Progression From Sequence

---
+++ 1502 can-make-arithmetic-progression-from-sequence/solution.go
@@ -0,0 +1,13 @@
+func canMakeArithmeticProgression(arr []int) bool {
+    if len(arr)<=2 {
+        return true
+    }
+    sort.Ints(arr)
+    gap:=arr[1]-arr[0]
+    for i:=2;i<len(arr);i++{
+        if arr[i]-arr[i-1]!=gap{
+            return false
+        }
+    }
+    return true
+}
