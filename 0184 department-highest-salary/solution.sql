31034048e665fe8b77b21e8c9af448d116dcc1ac
Feroz <mdferozahmed27156@gmail.com>
2026-02-23 08:21:24 +0000

    Solved Department Highest Salary

--- 0184 department-highest-salary/solution.sql
+++ 0184 department-highest-salary/solution.sql
@@ -1,19 +1,6 @@
-# Write your MySQL query statement below
-
-
-
--- select 
--- (select name from Department where id=e.departmentId) as Department,
--- e.name as Employee,e.salary as Salary from Employee as e where salary = (select max(salary) from Employee where departmentId=e.departmentId);
-
-
-
-
+-- Write your PostgreSQL query statement below
 select d.name as Department,e.name as Employee,
 e.salary as Salary
 from Employee as e left join Department as d on d.id=e.departmentId where e.salary in 
 (select max(salary) from employee WHERE departmentId = e.departmentId);
 ;
-
-
-
