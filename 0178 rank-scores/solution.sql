1c0a0f7a27776da65a55cb95e4bb4162ab0d8bf7
Feroz <mdferozahmed27156@gmail.com>
2026-02-23 08:10:12 +0000

    Solved Rank Scores

--- 0178 rank-scores/solution.sql
+++ 0178 rank-scores/solution.sql
@@ -1,9 +1,4 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
 
-
-
-SELECT 
-    score,
-    DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
-FROM Scores;
+select score,dense_rank() over (order by score desc) as rank from Scores;
