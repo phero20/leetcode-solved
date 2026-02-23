b32fdd83d095403f2e41d2b455bf8c76e633709e
Feroz <mdferozahmed27156@gmail.com>
2026-02-23 06:11:06 +0000

    Solved Permutation Sequence

---
+++ 0060 permutation-sequence/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+      List<Integer> li=new ArrayList<>();
+        for(int i=1;i<=n;i++) li.add(i);
+        int[] fact=new int[n+1];
+        fact[0]=1;
+        for(int i=1;i<=n;i++) fact[i]=fact[i-1]*i;
+        k--;
+        StringBuilder ans = new StringBuilder();
+        for(int i=n;i>=1;i--) {
+            int index=k/fact[i-1];
+            ans.append(li.get(index));
+            li.remove(index);
+            k%=fact[i-1];
+        }
+        return ans.toString(); 
+    }
+}
