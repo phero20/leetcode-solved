bbb4717a132fdfc89bec0bdece3c1cf05aee04b4
Feroz <mdferozahmed27156@gmail.com>
2026-02-23 08:31:05 +0000

    Solved Factorial Trailing Zeroes

--- 0172 factorial-trailing-zeroes/solution.java
+++ 0172 factorial-trailing-zeroes/solution.java
@@ -1,16 +1,10 @@
 class Solution {
     public int trailingZeroes(int n) {
-        if (n < 0) {
-            return 0;
+        int count=0;
+        while(n>0) {
+            n/=5;
+            count+=n;
         }
-
-        int count = 0;
-        // This loop adds n/5, then n/25, then n/125, etc.
-        while (n >= 5) {
-            n /= 5;
-            count += n;
-        }
-        
         return count;
     }
 }
