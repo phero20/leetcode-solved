be25345ce3e616b6f102e919a0fa5412f43efae3
Feroz <mdferozahmed27156@gmail.com>
2026-02-23 10:49:35 +0000

    Solved Concatenation of Array

---
+++ 1929 concatenation-of-array/solution.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int[] res = new int[2*nums.Length];
+        for(int i=0;i<nums.Length;i++) {
+            res[i]=nums[i];
+            res[i+nums.Length]=nums[i];
+        }
+        return res;
+    }
+}
