b244691f661e0f198b3935b4ec1fc9561e6af75b
Feroz <mdferozahmed27156@gmail.com>
2026-02-23 08:00:23 +0000

    Solved Movie Rating

---
+++ 1341 movie-rating/solution.sql
@@ -0,0 +1,22 @@
+-- Average rating in Feb 2020
+WITH top_user AS (
+    SELECT u.name
+    FROM Users u
+    JOIN MovieRating mr ON u.user_id = mr.user_id
+    GROUP BY u.user_id, u.name
+    ORDER BY COUNT(*) DESC, u.name ASC
+    LIMIT 1
+),
+top_movie AS (
+    SELECT m.title
+    FROM Movies m
+    JOIN MovieRating mr ON m.movie_id = mr.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.movie_id, m.title
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
+    LIMIT 1
+)
+SELECT name AS results FROM top_user
+UNION ALL
+SELECT title AS results FROM top_movie;
+
