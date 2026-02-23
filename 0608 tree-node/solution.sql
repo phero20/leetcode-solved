9a3ec2e8914f5cc77b06c720ad788cc3c0f09bdf
Feroz <mdferozahmed27156@gmail.com>
2026-02-23 08:07:26 +0000

    Solved Tree Node

--- 0608 tree-node/solution.sql
+++ 0608 tree-node/solution.sql
@@ -1,23 +1,13 @@
-# Write your MySQL query statement below
-
-
-
-
+-- Write your PostgreSQL query statement below
 
 
 select t1.id,
-case when
-    t1.p_id is null then 'Root'
-    when not exists(
-        select 1 from Tree t2 where t2.p_id=t1.id
-    )
-    then 'Leaf'
-    else 'Inner'
-END AS type
-FROM Tree t1;
-
-
-
-
-
-
+case when t1.p_id is null then 'Root'
+ when not exists (
+    select 1 from Tree t2 where t2.p_id=t1.id
+ )
+ then 'Leaf'
+ else 'Inner'
+ end as type
+ from Tree as t1;
+    
