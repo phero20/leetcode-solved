1f727e9209974ebd3775bc647240a91bca295c3b
Feroz <mdferozahmed27156@gmail.com>
2026-02-23 13:29:02 +0000

    Solved Counter II

---
+++ 2665 counter-ii/solution.ts
@@ -0,0 +1,28 @@
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+    let num=init;
+    return {
+        increment:()=>{
+            return ++num;
+        },
+        decrement:()=>{
+            return --num;
+        },
+        reset:()=>{
+            num=init;
+            return num;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
