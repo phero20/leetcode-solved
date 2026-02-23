fd4787f013fcfb3cb6ee326e204cb1186be3bb05
Feroz <mdferozahmed27156@gmail.com>
2026-02-23 05:29:58 +0000

    Solved Ugly Number

---
+++ 0263 ugly-number/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0) return false;
+        for(int i=2;i<=5;i++) {
+            while(n%i==0 && i!=4) n/=i;
+        }
+        return n==1;
+    }
+}
