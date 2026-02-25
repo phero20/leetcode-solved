50531bb9870ebbc5bd19bfa1b14bee9db3bab406
Feroz <mdferozahmed27156@gmail.com>
2026-02-25 08:09:34 +0000

    Solved Cache With Time Limit

--- 2622 cache-with-time-limit/solution.js
+++ 2622 cache-with-time-limit/solution.js
@@ -2,46 +2,28 @@ var TimeLimitedCache = function() {
     this.cache = {};
 };
 
-/** 
- * @param {number} key
- * @param {number} value
- * @param {number} duration time until expiration in ms
- * @return {boolean} if un-expired key already existed
- */
 TimeLimitedCache.prototype.set = function(key, value, duration) {
     const now = Date.now();
-    const existed = this.cache[key] !== undefined && this.cache[key].expiry > now;
-
-    this.cache[key] = {
-        value,
-        expiry: now + duration
-    };
-
-    return existed;
+    const existed = this.cache[key] !== undefined && this.cache[key].timeout > now;
+    this.cache[key] = { value, timeout: now + duration };
+    return existed;   // must explicitly return
 };
 
-/** 
- * @param {number} key
- * @return {number} value associated with key
- */
 TimeLimitedCache.prototype.get = function(key) {
     const now = Date.now();
-    if (this.cache[key] && this.cache[key].expiry > now) {
+    if (this.cache[key] && this.cache[key].timeout > now) {
         return this.cache[key].value;
     }
     return -1;
 };
 
-/** 
- * @return {number} count of non-expired keys
- */
 TimeLimitedCache.prototype.count = function() {
     const now = Date.now();
-    let count = 0;
-    for (const entry of Object.values(this.cache)) {
-        if (entry.expiry > now) {
-            count++;
+    let cnt = 0;
+    for (const key in this.cache) {
+        if (this.cache[key].timeout > now) {
+            cnt++;
         }
     }
-    return count;
+    return cnt;
 };
