d983a9998f711df0a206bf5afc7c3dbabe3d83e8
Feroz <mdferozahmed27156@gmail.com>
2026-02-25 08:18:46 +0000

    Solved Execute Asynchronous Functions in Parallel

--- 2721 execute-asynchronous-functions-in-parallel/solution.js
+++ 2721 execute-asynchronous-functions-in-parallel/solution.js
@@ -3,23 +3,24 @@
  * @return {Promise<any>}
  */
 var promiseAll = function(functions) {
-    return new Promise((resolve,reject)=>{
-        const res=[]
-    let completed = 0
-    functions.forEach((fn,ind)=>{
-        Promise.resolve()
-        .then(fn)
-        .then(val=>{
-            res[ind]=val
-            completed++
-            if(completed===functions.length) resolve(res)
-        }).catch(reject)
-    })
-    if (functions.length === 0) {
+   return new Promise((resolve,reject)=>{
+        let res =[]
+        let completed=0
+        if (functions.length === 0) {
             resolve([]);
-    }
+            return
+        }
+        functions.forEach((fn,ind)=>{
+            Promise.resolve()
+            .then(fn)
+            .then(val=>{
+                res[ind]=val
+                completed++
+                if(completed===functions.length) resolve(res)
+            }).catch(reject)
+        })
+        
     })
-    
 };
 
 /**
