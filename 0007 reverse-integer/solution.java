d3b4e35c6e058ea036c5902214566594ec4ea9aa
Feroz <mdferozahmed27156@gmail.com>
2026-02-26 11:24:06 +0000

    Solved Reverse Integer

--- 0007 reverse-integer/solution.java
+++ 0007 reverse-integer/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int reverse(int x) {
-        int rev=0;
-        while(x!=0){
-            int d=x%10;
-            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && d > 7)) {
-                return 0;
-            }
-            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && d < -8)) {
-                return 0;
-            }
-            rev=rev*10+d;
-            x/=10;
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+           if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10){
+            return 0;
+        }
+            rev = rev * 10 + pop;
+
         }
         return rev;
     }
