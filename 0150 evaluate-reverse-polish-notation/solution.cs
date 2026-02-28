c340913a547215853318c92aec54c23272a2de52
Feroz <mdferozahmed27156@gmail.com>
2026-02-28 14:15:18 +0000

    Solved Evaluate Reverse Polish Notation

--- 0150 evaluate-reverse-polish-notation/solution.cs
+++ 0150 evaluate-reverse-polish-notation/solution.cs
@@ -2,27 +2,22 @@ public class Solution {
     public int EvalRPN(string[] tokens) {
         Stack<int> st = new();
         int st1, st2;
-        foreach(String token in tokens) {
-            switch (token) {
-                case "+":
-                    st.Push(st.Pop() + st.Pop());
-                    break;
-                case "-":
-                    st2 = st.Pop();
-                    st1 = st.Pop();
-                    st.Push(st1 - st2);
-                    break;
-                case "*":
-                    st.Push(st.Pop() * st.Pop());
-                    break;
-                case "/":
-                    st2 = st.Pop();
-                    st1 = st.Pop();
-                    st.Push(st1 / st2);
-                    break;
-                default:
-                    st.Push(int.Parse(token));
-                    break;
+        foreach(var t in tokens) {
+            switch(t){
+                case "+" : st.Push(st.Pop()+st.Pop());
+                break;
+                case "-":  st2=st.Pop();
+                            st1=st.Pop();
+                            st.Push(st1-st2);
+                break;
+                case "*" : st.Push(st.Pop()*st.Pop());
+                break;
+                case "/" : st2=st.Pop();
+                            st1=st.Pop();
+                            st.Push(st1/st2);
+                break;
+                default: st.Push(int.Parse(t));
+                break;
             }
         }
         return st.Pop();
