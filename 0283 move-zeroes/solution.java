19a68c49b1cd1d6a7d5cee53f08e0fac7592b6ab
Feroz <mdferozahmed27156@gmail.com>
2026-02-28 00:49:42 +0000

    Solved Move Zeroes

--- 0283 move-zeroes/solution.java
+++ 0283 move-zeroes/solution.java
@@ -1,24 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-    //    int i=0,j=0;
-    //    while(j<nums.length) {
-    //     if(nums[i]==0 && nums[j]!=0) {
-    //         nums[i++]=nums[j];
-    //         nums[j]=0;
-    //     }
-    //     if(nums[i]!=0) i++;
-    //     j++;
-    //    }
-
-
-
-    int i=0;
-    for(int j=0;j<nums.length;j++) {
-        if(nums[j]!=0) {
-            int temp=nums[i];
-            nums[i++]=nums[j];
-            nums[j]=temp;
+        int l=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]!=0) {
+                int temp=nums[l];
+                nums[l++]=nums[r];
+                nums[r]=temp;
+            }
         }
-    }
     }
 }
