bd6b17a5405452a4d2fdaaf42335fd2ebc739b34
Feroz <mdferozahmed27156@gmail.com>
2026-02-28 05:06:05 +0000

    Solved Sort an Array

---
+++ 0912 sort-an-array/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int n=nums.length-1;
+        mergeSort(nums,0,n);
+        return nums;
+    }
+    private void mergeSort(int[] nums,int left,int right) {
+        if(left>=right) return;
+        int mid=left+(right-left)/2;
+        mergeSort(nums,left,mid);
+        mergeSort(nums,mid+1,right);
+        merge(nums,left,mid,right);
+    }
+    private void merge(int[] nums,int left,int mid,int right) {
+        int i=left,j=mid+1,k=0;
+        int res[] = new int[right-left+1];
+        while(i<=mid && j<=right) {
+            if(nums[i]<=nums[j]) res[k++]=nums[i++];
+            else res[k++]=nums[j++];
+        }
+       while (i <= mid) res[k++] = nums[i++];
+        while (j <= right) res[k++] = nums[j++];
+        for (int t = 0; t < res.length; t++) {
+            nums[left + t] = res[t];
+        }
+
+    }
+}
