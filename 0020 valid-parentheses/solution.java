4a01a86d70fc02654d7fe67ccc7cdb559ee0d4d1
Feroz <mdferozahmed27156@gmail.com>
2026-02-28 12:44:46 +0000

    Solved Valid Parentheses

--- 0020 valid-parentheses/solution.java
+++ 0020 valid-parentheses/solution.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if((ch=='[')||(ch=='{')||(ch=='(')){
-                stack.push(ch);
-            }else{
- if(stack.isEmpty()) return false;
-            char top = stack.pop();
-            if((top=='[' && s.charAt(i)!=']') || (top=='{' && s.charAt(i)!='}') || (top=='(' && s.charAt(i)!=')')){
-                return false;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else {
+                if (stack.isEmpty())
+                    return false;
+                else if ((c == ')' && stack.peek() == '(') ||
+                        ((c == '}') && stack.peek() == '{') ||
+                        ((c == ']') && stack.peek() == '['))
+                    stack.pop();
+                else
+                    return false;
             }
         }
-            }
-           
         return stack.isEmpty();
     }
 }
