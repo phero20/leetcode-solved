2171f5ee68dbdd917b4033825d6dd303c5f94901
Feroz <mdferozahmed27156@gmail.com>
2026-02-28 01:01:13 +0000

    Solved Single Number

--- 0136 single-number/solution.java
+++ 0136 single-number/solution.java
@@ -1,10 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-        int s=nums[0];
-        for(int i=1;i<n;i++){
-           s = s ^ nums[i];
-        }
-        return s;
+        int res=0;
+        for(int i:nums) res^=i;
+        return res;
     }
 }
