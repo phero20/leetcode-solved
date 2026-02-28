5ca6dfc60ae4a2e7a708822b409ce02061afcaed
Feroz <mdferozahmed27156@gmail.com>
2026-02-28 15:16:50 +0000

    Solved Daily Temperatures

--- 0739 daily-temperatures/solution.cs
+++ 0739 daily-temperatures/solution.cs
@@ -1,13 +1,12 @@
 public class Solution {
     public int[] DailyTemperatures(int[] t) {
         Stack<int> st= new();
-        int[] res=new int[t.Length];
-        for(int i=0;i<t.Length;i++){
-            while(st.Count>0 && t[st.Peek()]<t[i]){
-                res[st.Peek()]=i-st.Pop();
-            }
-            st.Push(i);
-        }
-        return res;
+       int[] res = new int[t.Length];
+       for(int i=0;i<t.Length;i++) {
+        while(st.Count>0 && t[st.Peek()]<t[i])
+            res[st.Peek()]=i-st.Pop();
+        st.Push(i);
+       }
+         return res;
     }
 }
