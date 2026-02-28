74aeae32635a77aa34a9f2b7a1092420d1f06073
Feroz <mdferozahmed27156@gmail.com>
2026-02-28 06:34:37 +0000

    Solved Longest Common Prefix

--- 0014 longest-common-prefix/solution.java
+++ 0014 longest-common-prefix/solution.java
@@ -1,12 +1,13 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-     String prefix = strs[0];
-     for(int i=1;i<strs.length;i++){
-      while(strs[i].indexOf(prefix)!=0){
-        prefix = prefix.substring(0,prefix.length()-1);
-        if(prefix.isEmpty()) return "";
-      }
-     }
-     return prefix;
+    public String longestCommonPrefix(String[] s) {
+        String first = s[0];
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<s.length;i++) {
+            while(s[i].indexOf(first)!=0) {
+                first = first.substring(0,first.length()-1);
+                if(first.isEmpty()) return "";
+            }
+        }
+        return first;
     }
 }
