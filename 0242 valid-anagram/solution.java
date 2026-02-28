eb53248a2b7679b3147a3e4ef8d766ed4b6daea6
Feroz <mdferozahmed27156@gmail.com>
2026-02-28 02:29:06 +0000

    Solved Valid Anagram

--- 0242 valid-anagram/solution.java
+++ 0242 valid-anagram/solution.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())    return false;
-       int[] arr = new int[26];
-       for(char c:s.toCharArray()) arr[c-'a']++;
-        for(char c:t.toCharArray()) arr[c-'a']--;
-       for(int i:arr) if(i!=0) return false;
+        if (s.length() != t.length()) return false;
+        int[] ar = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            ar[s.charAt(i) - 'a']++;
+            ar[t.charAt(i) - 'a']--;
+        }
+        for (int i : ar) if (i != 0) return false;
         return true;
     }
 }
