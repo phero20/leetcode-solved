ef22563ed7884fc25b2ae6f5182f72d9f03fb90c
Feroz <mdferozahmed27156@gmail.com>
2026-02-28 00:05:11 +0000

    Solved Remove Element

--- 0027 remove-element/solution.java
+++ 0027 remove-element/solution.java
@@ -1,9 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val) nums[k++]=nums[i];
-        }
-        return k;
+        int l=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[
+               r]!=val)
+            {
+                nums[l++]=nums[r];
+                
+            }
+                }
+        return l;
     }
 }
