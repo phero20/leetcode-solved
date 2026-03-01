83b788cdcee4eb4293821e35822d0adbc3cca82d
Feroz <mdferozahmed27156@gmail.com>
2026-03-01 15:56:35 +0000

    Solved Remove Element

--- 0027 remove-element/solution.cs
+++ 0027 remove-element/solution.cs
@@ -1,11 +1,9 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        int j=0;
-        for(int i=0;i<nums.Length;i++) {
-            if(nums[i]!=val) {
-                nums[j++]=nums[i];
-            }
+        int i=0;
+        for(int j=0;j<nums.Length;j++) {
+            if(nums[j]!=val) nums[i++]=nums[j];
         }
-        return j;
+        return i;
     }
 }
