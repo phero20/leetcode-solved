e8b3adbd99523e40ef5d326a3ed7b6744bd2cd59
Feroz <mdferozahmed27156@gmail.com>
2026-03-01 07:25:50 +0000

    Solved Search in Rotated Sorted Array

---
+++ 0033 search-in-rotated-sorted-array/solution.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int low=0,high=nums.Length-1;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[low]<=nums[mid]) {
+                if(target>= nums[low] && target<nums[mid]) high=mid-1; 
+                else low=mid+1;
+            } else {
+                if(target> nums[mid] && target<=nums[high]) low=mid+1; 
+                else high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
