74fbcf039affe49b47b6a61979845b641d07e8de
Feroz <mdferozahmed27156@gmail.com>
2026-03-01 06:42:15 +0000

    Solved Sqrt(x)

--- 0069 sqrtx/solution.java
+++ 0069 sqrtx/solution.java
@@ -1,5 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+      int low=0,high=x;
+      while(low<=high) {
+        int mid=low+(high-low)/2;
+        long sq = (long) mid * mid;
+        if(sq==x) return mid;
+        else if(sq<x) low=mid+1;
+        else high=mid-1;
+      }
+      return high;
     }
 }
