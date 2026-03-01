e90489ac8307140e93934cfe825b34a58b8024d8
Feroz <mdferozahmed27156@gmail.com>
2026-03-01 15:30:59 +0000

    Solved Binary Search

---
+++ 0704 binary-search/solution.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int l=0,r=nums.Length-1;
+        while(l<=r) {
+            int mid = l+(r-l)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) l=mid+1;
+            else r=mid-1;
+        }
+        return -1;
+    }
+}
