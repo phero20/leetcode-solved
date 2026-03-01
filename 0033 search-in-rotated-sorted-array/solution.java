a2818a543956fd860b4e1db77af7a7f4d2b98574
Feroz <mdferozahmed27156@gmail.com>
2026-03-01 07:41:32 +0000

    Solved Search in Rotated Sorted Array

--- 0033 search-in-rotated-sorted-array/solution.java
+++ 0033 search-in-rotated-sorted-array/solution.java
@@ -1,31 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return mbin(nums,target,0,nums.length-1);
-    }
-
-    public int mbin(int arr[],int target,int left,int right){
-        if(left>right){
-            return -1;
+        int low=0,high=nums.length-1;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[low]<=nums[mid]) {
+                if(target >= nums[low] && target < nums[mid]) high=mid-1;
+                else low=mid+1;
+            } else {
+                if(target > nums[mid] && target <= nums[high]) low=mid+1;
+                else high=mid-1;
+            }
         }
-        int mid = left + ((right-left) /2);
-        if(arr[mid]==target){
-            return mid;
-        }
-        if(arr[mid]>=arr[left]){
-         if(arr[left]<=target && target<=arr[mid]){
-           return mbin(arr,target,left,mid-1);
-        }
-        else{
-            return mbin(arr,target,mid+1,right);
-        }
-        }
-        else{
-             if(arr[mid]<=target && target<=arr[right]){
-           return mbin(arr,target,mid+1,right);
-        }
-        else{
-            return mbin(arr,target,left,mid-1);
-        }
-        }
+        return -1;
     }
 }
