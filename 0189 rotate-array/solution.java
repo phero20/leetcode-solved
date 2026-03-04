3162f19bc6cc05b8ebecd32534cd78532a1fb8f8
Feroz <mdferozahmed27156@gmail.com>
2026-03-04 06:44:50 +0000

    Solved Rotate Array

--- 0189 rotate-array/solution.java
+++ 0189 rotate-array/solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % nums.length;
+        k=k%nums.length;
         rev(nums,0,nums.length-1);
         rev(nums,0,k-1);
         rev(nums,k,nums.length-1);
     }
-
-    private void rev(int nums[],int i,int j) {
-        while(i<j) {
-            int temp = nums[i];
+    private void rev(int[]nums,int i,int j) {
+        while(i<=j) {
+            int temp=nums[i];
             nums[i++]=nums[j];
             nums[j--]=temp;
         }
