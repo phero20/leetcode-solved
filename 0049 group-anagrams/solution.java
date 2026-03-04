9c2d707bed93d83784ef5dfe643015df81cf9025
Feroz <mdferozahmed27156@gmail.com>
2026-03-04 11:43:07 +0000

    Solved Group Anagrams

--- 0049 group-anagrams/solution.java
+++ 0049 group-anagrams/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
     Map<Integer,List<String>> map = new HashMap<>();
-    for(String s:strs){
-        int key=getKey(s);
-        map.putIfAbsent(key,new ArrayList<>());
-        map.get(key).add(s);
+    for(String s:strs) {
+        int key = getKey(s);
+        if(!map.containsKey(key)) {
+            map.put(key,new ArrayList<>());
+        }
+        map.get(key).add(s); 
     }
     return new ArrayList<>(map.values());
     }
-
-    private int getKey(String s){
-        int[] arr=new int[26];
-        for(char c:s.toCharArray()) arr[c-'a']++;
-        return Arrays.hashCode(arr);
+    private int getKey(String s) {
+        int[] d=new int[26];
+        for(char c:s.toCharArray()) d[c-'a']++;
+        return Arrays.hashCode(d);
     }
 }
