c6b59c1b04de2ba00ad9671dfc5d6007c6d70624
Feroz <mdferozahmed27156@gmail.com>
2026-03-04 07:12:20 +0000

    Solved Pascal's Triangle

--- 0118 pascals-triangle/solution.java
+++ 0118 pascals-triangle/solution.java
@@ -1,16 +1,15 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
+    public List<List<Integer>> generate(int n) {
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0;i<numRows;i++) {
-            List<Integer> l = new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            List<Integer> ians = new ArrayList<>();
             for(int j=0;j<=i;j++) {
-                if(j==0 || j==i) l.add(1);
-                else {
-                    int n=ans.get(i-1).get(j)+ans.get(i-1).get(j-1);
-                    l.add(n);
-                }
+            if(j==0 || j==i) ians.add(1);
+            else {
+                ians.add(ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
             }
-            ans.add(l);
+            }
+            ans.add(ians);
         }
         return ans;
     }
