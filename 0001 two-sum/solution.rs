8413a0e396e177318206c7b69e8da4dc4ae272a5
Feroz <mdferozahmed27156@gmail.com>
2026-03-04 08:26:02 +0000

    Solved Two Sum

---
+++ 0001 two-sum/solution.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+
+        for (i, &num) in nums.iter().enumerate() {
+            let diff = target - num;
+
+            if let Some(&index) = map.get(&diff) {
+                return vec![index as i32, i as i32];
+            }
+
+            map.insert(num, i);
+        }
+
+        vec![]
+    }
+}
