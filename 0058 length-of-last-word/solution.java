9dae0134049c3a69e22425add9fe3ad97f016d8b
Feroz <mdferozahmed27156@gmail.com>
2026-03-05 12:22:48 +0000

    Solved Length of Last Word

--- 0058 length-of-last-word/solution.java
+++ 0058 length-of-last-word/solution.java
@@ -1,11 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int l=0,i=s.length()-1;
-        while(s.charAt(i)==' ') i--;
-        while(i>=0 && s.charAt(i)!=' ') {
-            i--;
-            l++;
-        }
-        return l;
+        s=s.trim();
+        int i=s.length()-1;
+        while(i>=0 && s.charAt(i)!=' ') i--;
+        return s.length()-i-1;
     }
 }
