0a25a6697ff1111b27f0907b1a99244138a00329
Feroz <mdferozahmed27156@gmail.com>
2026-03-05 12:09:26 +0000

    Solved Append Characters to String to Make Subsequence

---
+++ 2486 append-characters-to-string-to-make-subsequence/solution.go
@@ -0,0 +1,9 @@
+func appendCharacters(s string, t string) int {
+    j := 0
+    for i := 0; i < len(s) && j < len(t); i++ {
+        if s[i] == t[j] {
+            j++
+        }
+    }
+    return len(t) - j
+}
