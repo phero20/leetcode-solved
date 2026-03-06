f730eddb0003794480fcebe91ffb14e061562419
Feroz <mdferozahmed27156@gmail.com>
2026-03-06 05:19:33 +0000

    Solved Product of Array Except Self

---
+++ 0238 product-of-array-except-self/solution.rs
@@ -0,0 +1,23 @@
+
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+
+        let n = nums.len();
+        let mut ans = vec![0; n];
+
+        ans[0] = 1;
+
+        for i in 1..n {
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+
+        let mut suffix = 1;
+
+        for i in (0..n).rev() {
+            ans[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        ans
+    }
+}
