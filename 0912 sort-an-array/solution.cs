1cb79c4a6bd593a0a523b09afb0d71646d1b3e5c
Feroz <mdferozahmed27156@gmail.com>
2026-03-06 04:11:00 +0000

    Solved Sort an Array

--- 0912 sort-an-array/solution.cs
+++ 0912 sort-an-array/solution.cs
@@ -1,29 +1,25 @@
 public class Solution {
     public int[] SortArray(int[] nums) {
-        Array.Sort(nums);
-        // mergeSort(nums,0,nums.Length-1);
+        // Array.Sort(nums);
+        mergeSort(nums,0,nums.Length-1);
         return nums;
     }
-    private void mergeSort(int[] nums,int low,int high) {
-        if(low>=high) return;
-        int mid = low+(high-low)/2;
-        mergeSort(nums,low,mid);
-        mergeSort(nums,mid+1,high);
-        merge(nums,low,mid,high);
+    private void mergeSort(int[] nums,int left,int right) {
+        if(left>=right) return;
+        int mid=left+(right-left)/2;
+        mergeSort(nums,left,mid);
+        mergeSort(nums,mid+1,right);
+        merge(nums,left,mid,right);
     }
-    private void merge(int[] nums,int low,int mid,int high) {
-        int i=low,j=mid+1,k=0;
-        int[] res = new int[high-low+1];
-        while(i<=mid && j<=high) {
-            if(nums[i]<nums[j]) res[k++]=nums[i++];
+    private void merge(int[] nums,int left,int mid,int right) {
+        int i=left,j=mid+1,k=0;
+        int[] res = new int[right-left+1];
+        while(i<=mid && j<=right) {
+            if(nums[i]<nums[j]) res[k++]=nums[i++]; 
             else res[k++]=nums[j++];
         }
-
-        while (i <= mid) res[k++] = nums[i++];
-        while (j <= high) res[k++] = nums[j++];
-
-        for(int t=0;t<res.Length;t++) {
-            nums[low+t]=res[t];
-        }
+        while(i<=mid) res[k++]=nums[i++];
+        while(j<=right) res[k++]=nums[j++];
+        for(i=0;i<res.Length;i++) nums[left+i]=res[i];
     }
 }
