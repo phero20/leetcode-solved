9e3addf0c96b2d5d4f004da31852b80d33dd8b1c
Feroz <mdferozahmed27156@gmail.com>
2026-03-06 04:51:08 +0000

    Solved Top K Frequent Elements

--- 0347 top-k-frequent-elements/solution.java
+++ 0347 top-k-frequent-elements/solution.java
@@ -1,14 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer,Integer> map = new HashMap<>();
-        for(int i:nums) map.merge(i,1,Integer::sum);
-        PriorityQueue<Map.Entry<Integer,Integer>> maxHeap = new PriorityQueue<>((e1,e2)->e2.getValue()-e1.getValue());
-        maxHeap.addAll(map.entrySet());
-        int res[] = new int[k];
-        int j=0;
-        while(j<k) {
-            Map.Entry<Integer,Integer> entry = maxHeap.poll();
-            res[j++]=entry.getKey();
+        for(int i:nums) map.put(i,map.getOrDefault(i,0)+1);
+        List<Integer>[] b = new List[nums.length+1];
+        for(int key:map.keySet()) {
+            int val=map.get(key);
+            if(b[val]==null) b[val]=new ArrayList<>();
+            b[val].add(key);
+        }
+        int[] res = new int[k];
+        int idx=0;
+        for(int i=b.length-1;i>=0 && idx<k;i--) {
+            if(b[i]!=null) {
+                for(int num:b[i]) {
+                    res[idx++]=num;
+                    if(idx==k) break;
+                }
+            } 
         }
         return res;
     }
