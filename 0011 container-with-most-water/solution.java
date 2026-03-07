7bc1eb1101d44bd3627b69beec079eac556edced
Feroz <mdferozahmed27156@gmail.com>
2026-03-07 03:36:41 +0000

    Solved Container With Most Water

--- 0011 container-with-most-water/solution.java
+++ 0011 container-with-most-water/solution.java
@@ -1,14 +1,10 @@
 class Solution {
     public int maxArea(int[] height) {
-        int i = 0, j = height.length - 1, max = 0;
-        while (i < j) {
-            int area = (j - i) * Math.min(height[i], height[j]);
-            max = Math.max(max, area);
-            if (height[i] < height[j]) {
-                i++;
-            } else {
-                j--;
-            }
+        int i=0,j=height.length-1,max=0;
+        while(i<j) {
+            max=Math.max(max,(j-i)*Math.min(height[i],height[j]));
+            if(height[i]>height[j]) j--;
+            else i++;
         }
         return max;
     }
