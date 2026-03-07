bd84f743854915f59742dda26c4696a2f0d4b1f7
Feroz <mdferozahmed27156@gmail.com>
2026-03-07 03:22:40 +0000

    Solved 3Sum

--- 0015 3sum/solution.java
+++ 0015 3sum/solution.java
@@ -6,17 +6,17 @@ class Solution {
             if(i>0 && nums[i]==nums[i-1]) continue;
             int l=i+1,r=nums.length-1;
             while(l<r) {
-                int sum=nums[i]+nums[l]+nums[r];
-                if (sum==0) {
-                    ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                int sum = nums[i]+nums[l]+nums[r];
+                if(sum==0) {
+                    ans.add(Arrays.asList(nums[i],nums[l],nums[r]));
                     while(l<r && nums[l]==nums[l+1]) l++;
                     while(l<r && nums[r]==nums[r-1]) r--;
                     l++;
                     r--;
                 }
-                else if (sum<0) l++;
+                else if(sum<0) l++;
                 else r--;
-             }
+            }
         }
         return ans;
     }
