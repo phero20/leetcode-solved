ffc35350c283549ae86d7bae441c03bf412ca875
Feroz <mdferozahmed27156@gmail.com>
2026-03-07 03:31:04 +0000

    Solved 4Sum

--- 0018 4sum/solution.java
+++ 0018 4sum/solution.java
@@ -2,15 +2,17 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        for (int k = 0; k < nums.length-3; k++) {
-            if (k>0 && nums[k]==nums[k-1]) continue;
-            for(int i=k+1;i<nums.length-2;i++) {
-                if(i>k+1 && nums[i]==nums[i-1]) continue;
-                int l = i + 1, r = nums.length - 1;
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i+1 && nums[j] == nums[j - 1])
+                    continue;
+                int l = j + 1, r = nums.length - 1;
                 while (l < r) {
-                    long sum = (long)nums[i] + nums[l] + nums[r]+nums[k];
+                    long sum = (long)nums[i] + nums[j] + nums[l] + nums[r];
                     if (sum == target) {
-                        ans.add(Arrays.asList(nums[k],nums[i], nums[l], nums[r]));
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
                         while (l < r && nums[l] == nums[l + 1])
                             l++;
                         while (l < r && nums[r] == nums[r - 1])
@@ -24,7 +26,6 @@ class Solution {
                 }
             }
         }
-
         return ans;
     }
 }
