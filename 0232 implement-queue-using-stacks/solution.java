e714232eb1f18895bc4ae9c674812ae9473554c3
Feroz <mdferozahmed27156@gmail.com>
2026-03-07 04:51:41 +0000

    Solved Implement Queue using Stacks

--- 0232 implement-queue-using-stacks/solution.java
+++ 0232 implement-queue-using-stacks/solution.java
@@ -1,23 +1,35 @@
 class MyQueue {
-    private ArrayList<Integer> queue;
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
     public MyQueue() {
-        queue=new ArrayList<>();
+       s1=new Stack<>();
+       s2=new Stack<>();
     }
     
     public void push(int x) {
-        queue.add(x);
+        s1.push(x);
     }
     
     public int pop() {
-       return queue.remove(0); 
+         if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
     }
     
     public int peek() {
-        return queue.get(0);
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
     }
     
     public boolean empty() {
-        return queue.isEmpty();
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
 
