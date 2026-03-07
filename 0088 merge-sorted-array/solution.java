811f212a1f1603762232987c059dafa24a7c9a41
Feroz <mdferozahmed27156@gmail.com>
2026-03-07 03:04:24 +0000

    Solved Merge Sorted Array

--- 0088 merge-sorted-array/solution.java
+++ 0088 merge-sorted-array/solution.java
@@ -1,9 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int l = m - 1, r = n - 1, t = m + n - 1;
-        while (r >= 0) {
-            if (l >= 0 && nums1[l] > nums2[r]) nums1[t--] = nums1[l--];
-            else nums1[t--] = nums2[r--];
+        int len = m + n - 1;
+        m--;
+        n--;
+        while (n >= 0) {
+            if (m >= 0 && nums1[m] > nums2[n])
+                nums1[len--] = nums1[m--];
+            else
+                nums1[len--] = nums2[n--];
         }
     }
 }
