eb903d146a779ff56dea4b312e57066441db548f
Feroz <mdferozahmed27156@gmail.com>
2026-03-09 02:39:30 +0000

    Solved Sqrt(x)

---
+++ 0069 sqrtx/solution.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MySqrt(int x) {
+        int l=0,r=x;
+        while(l<=r) {
+            int mid=l+(r-l)/2;
+            long sq = (long)mid*mid;
+            if(sq==x) return mid;
+            else if(sq<x) l=mid+1;
+            else r=mid-1;
+        }
+        return r;
+    }
+}
