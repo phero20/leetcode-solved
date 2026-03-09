8fca249287f8b88fae51181428a6c976310afd8d
Feroz <mdferozahmed27156@gmail.com>
2026-03-09 03:53:27 +0000

    Solved Search in Rotated Sorted Array

---
+++ 0033 search-in-rotated-sorted-array/solution.go
@@ -0,0 +1,22 @@
+func search(nums []int, target int) int {
+    low,high:=0,len(nums)-1
+    for low<=high {
+        mid:=low+(high-low)/2
+        if nums[mid]==target {
+            return mid
+        } else if nums[low]<=nums[mid] {
+            if target >= nums[low] && target < nums[mid] {
+                high=mid-1
+            } else {
+                low=mid+1
+            }
+        } else {
+            if target > nums[mid] && target<=nums[high] {
+                low=mid+1
+            } else {
+                high=mid-1
+            }
+        }
+    }
+    return -1
+}
