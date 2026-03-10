af66adbd1703878d5166549bc19a8a5fb413a0bf
Feroz <mdferozahmed27156@gmail.com>
2026-03-10 03:58:12 +0000

    Solved Median of Two Sorted Arrays

--- 0004 median-of-two-sorted-arrays/solution.java
+++ 0004 median-of-two-sorted-arrays/solution.java
@@ -1,20 +1,32 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i=0,j=0,k=0;
-        int[] merge = new int[nums1.length+nums2.length];
-        while(i<nums1.length && j<nums2.length) {
-            if(nums1[i]<nums2[j]) {
-                merge[k++]=nums1[i++];
+        if (nums1.length > nums2.length)
+            return findMedianSortedArrays(nums2, nums1);
+        int m = nums1.length;
+        int n = nums2.length;
+        int low = 0, high = m;
+        int half = (m + n + 1) / 2;
+
+        while (low <= high) {
+            int l = low + (high - low) / 2;
+            int r = half - l;
+            int maxleft1 = (l == 0) ? Integer.MIN_VALUE : nums1[l - 1];
+            int minright1 = (l == m) ? Integer.MAX_VALUE : nums1[l];
+
+            int maxleft2 = (r == 0) ? Integer.MIN_VALUE : nums2[r - 1];
+            int minright2 = (r == n) ? Integer.MAX_VALUE : nums2[r];
+            if (maxleft1 <= minright2 && maxleft2 <= minright1) {
+                if ((m + n) % 2 != 0) {
+                    return Math.max(maxleft1, maxleft2);
+                } else {
+                    return (Math.max(maxleft1, maxleft2) + Math.min(minright1, minright2)) / 2.0;
+                }
+            } else if (maxleft1 > minright2) {
+                high = l - 1;
             } else {
-                merge[k++]=nums2[j++];
+                low = l + 1;
             }
         }
-        while(i<nums1.length) merge[k++]=nums1[i++];
-        while(j<nums2.length) merge[k++]=nums2[j++];
-
-        if(merge.length % 2 ==0) {
-            return (double)(merge[merge.length/2-1]+merge[merge.length/2])/2;
-        }
-            return merge[merge.length/2];
+        return 0.0;
     }
 }
