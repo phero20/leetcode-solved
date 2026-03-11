c7760f495b5bf8a2239c7bb62d427405d119fcbf
Feroz <mdferozahmed27156@gmail.com>
2026-03-11 04:12:47 +0000

    Solved Permutation in String

--- 0567 permutation-in-string/solution.java
+++ 0567 permutation-in-string/solution.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-    if(s2.length()<s1.length()) return false;
-    int c1[] = new int[126];
-    int c2[] = new int[126];
-    int k=s1.length();
-    for(int i=0;i<k;i++) {
-        c1[s1.charAt(i)]++;
-        c2[s2.charAt(i)]++;
-    }
-     if(Arrays.equals(c1, c2)) return true;
-    for(int i=k;i<s2.length();i++) {
-        c2[s2.charAt(i)]++;
-        c2[s2.charAt(i-k)]--;
-        if(Arrays.equals(c1, c2)) return true;
-    }
-    return false;
+        if (s2.length() < s1.length())
+            return false;
+        int[] c1 = new int[126];
+        int[] c2 = new int[126];
+        int k = s1.length();
+        for (int i = 0; i < k; i++) {
+            c1[s1.charAt(i)]++;
+            c2[s2.charAt(i)]++;
+        }
+        if (Arrays.equals(c1, c2))
+            return true;
+        for (int r = k; r < s2.length(); r++) {
+            c2[s2.charAt(r)]++;
+            c2[s2.charAt(r-k)]--;
+            if (Arrays.equals(c1, c2))
+                return true;
+        }
+        return false;
     }
 }
