937d110540ef9dc71a943cb5b4b92424093efa72
Feroz <mdferozahmed27156@gmail.com>
2026-03-11 03:30:22 +0000

    Solved Contains Duplicate II

--- 0219 contains-duplicate-ii/solution.java
+++ 0219 contains-duplicate-ii/solution.java
@@ -3,10 +3,8 @@ class Solution {
         Set<Integer> set = new HashSet<>();
 
         for(int i=0;i<nums.length;i++) {
-            if(i>k) {
-                set.remove(nums[i-k-1]);
-            }
-            if(!set.add(nums[i])) return true;
+            if(i>k) set.remove(nums[i-k-1]);
+            if(!set.add(nums[i])) return true; 
         }
         return false;
     }
