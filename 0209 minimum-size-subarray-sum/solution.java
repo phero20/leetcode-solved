353204dde63a406d1a53122fe8a35f5c24159dcc
Feroz <mdferozahmed27156@gmail.com>
2026-03-11 04:33:42 +0000

    Solved Minimum Size Subarray Sum

--- 0209 minimum-size-subarray-sum/solution.java
+++ 0209 minimum-size-subarray-sum/solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int r=0,l=0,m=Integer.MAX_VALUE,sum=0;
-        while(r<nums.length) {
+        int l=0,sum=0,le=Integer.MAX_VALUE;
+        for(int r=0;r<nums.length;r++) {
             sum+=nums[r];
             while(sum>=target) {
-                m=Math.min(m,r-l+1);
+                le=Math.min(r-l+1,le);
                 sum-=nums[l++];
             }
-            r++;
         }
-        return m==Integer.MAX_VALUE ? m=0 : m;
+        return le==Integer.MAX_VALUE ? 0 : le;
     }
 }
