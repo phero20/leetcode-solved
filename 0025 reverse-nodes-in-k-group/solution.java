32b4051d9fde39c8dd4e2c1d8a59f36cd15cff5e
Feroz <mdferozahmed27156@gmail.com>
2026-03-16 08:02:34 +0000

    Solved Reverse Nodes in k-Group

---
+++ 0025 reverse-nodes-in-k-group/solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while (count < k && curr != null) {
+            count++;
+            curr = curr.next;
+        }
+        if (count == k) {
+            ListNode revHead = rev(head, k);
+
+            head.next = reverseKGroup(curr, k);
+            return revHead;
+        }
+        return head;
+    }
+
+    public ListNode rev(ListNode head, int k) {
+        ListNode prev = null, curr = head;
+        while (k > 0) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            k--;
+        }
+        return prev;
+    }
+}
