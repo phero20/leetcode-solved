a79f9958aae18ea69618ef50bf8e0a3e5ac9bc91
Feroz <mdferozahmed27156@gmail.com>
2026-03-17 05:30:48 +0000

    Solved Invert Binary Tree

---
+++ 0226 invert-binary-tree/solution.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if(root==null) return null;
+        invert(root);
+        return root;
+    }
+    private void invert(TreeNode root) {
+        if(root==null) return;
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        invert(root.left);
+        invert(root.right);
+    }
+}
