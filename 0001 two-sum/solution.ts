864ec70a48e3a3d41662822e9f3d5e0211883247
Feroz <mdferozahmed27156@gmail.com>
2026-03-22 06:39:03 +0000

    Solved Two Sum

---
+++ 0001 two-sum/solution.ts
@@ -0,0 +1,9 @@
+function twoSum(nums: number[], target: number): number[] {
+    let map = new Map<number, number>();
+    for(let i=0;i<nums.length;i++) {
+        let diff = target-nums[i];
+        if(map.has(diff)) return [i, map.get(diff)!];
+        map.set(nums[i],i);
+    }
+    return [];
+};
