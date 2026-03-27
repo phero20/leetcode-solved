76b7bead70b54e08e04f377c564100bf6ac9dafc
Feroz <mdferozahmed27156@gmail.com>
2026-03-27 06:57:54 +0000

    Solved How Many Numbers Are Smaller Than the Current Number

--- 1365 how-many-numbers-are-smaller-than-the-current-number/solution.java
+++ 1365 how-many-numbers-are-smaller-than-the-current-number/solution.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int res[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++) {
-            int c=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]<nums[i]) c++;
+        int ans[] = nums.clone();
+        Arrays.sort(ans);
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<ans.length;i++) {
+            if(!map.containsKey(ans[i])) {
+                map.put(ans[i],i);
             }
-            res[i]=c;
         }
-        return res;
+        for(int i=0;i<nums.length;i++) {
+            nums[i] = map.get(nums[i]);
+        }
+        return nums;
     }
 }
