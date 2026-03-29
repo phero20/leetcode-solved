72e0ad9fc34bb873319daf29c1cd375ded2bfbc7
Feroz <mdferozahmed27156@gmail.com>
2026-03-29 06:36:31 +0000

    Solved First Missing Positive

---
+++ 0041 first-missing-positive/solution.ts
@@ -0,0 +1,13 @@
+function firstMissingPositive(nums: number[]): number {
+    for(let i=0;i<nums.length;i++) {
+        while(  nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i]-1] ) {
+            let temp = nums[i];
+            nums[i] = nums[temp-1];
+            nums[temp-1] = temp;
+        }
+    }
+    for(let i=0;i<nums.length;i++) {
+        if(nums[i]!=i+1) return i+1;
+    }
+    return nums.length+1;
+};
