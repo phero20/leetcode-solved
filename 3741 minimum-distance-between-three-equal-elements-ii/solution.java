d7cb92f7fe3722af95435e6e9dbd58b74162d8be
Feroz <mdferozahmed27156@gmail.com>
2026-04-07 13:29:11 +0000

    Solved Minimum Distance Between Three Equal Elements II

---
+++ 3741 minimum-distance-between-three-equal-elements-ii/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        HashMap<Integer,List<Integer>> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++) map.computeIfAbsent(nums[i],k->new ArrayList<>()).add(i);
+        int ans = Integer.MAX_VALUE;
+        for(List<Integer> list : map.values()) {
+            if(list.size()>=3) {
+                Collections.sort(list);
+                for(int i=0;i+2<list.size();i++) {
+                        int j = list.get(i);
+                        int k = list.get(i+2);
+                        int d = 2*(k-j);
+                        ans = Math.min(ans,d);
+                }
+            }
+        }
+        return ans==Integer.MAX_VALUE ? -1 : ans;
+    }
+}
