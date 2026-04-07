4dea38b1d71771c3a76721f454494be8d1c89b4a
Feroz <mdferozahmed27156@gmail.com>
2026-04-07 12:41:02 +0000

    Solved Search Insert Position

---
+++ 0035 search-insert-position/solution.ts
@@ -0,0 +1,10 @@
+function searchInsert(nums: number[], target: number): number {
+    let l = 0,r = nums.length-1;
+    while(l <= r) {
+        let mid = Math.floor(l+(r-l)/2);
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] < target) l = mid+1;
+        else r = mid-1;
+    }
+    return l;
+};
