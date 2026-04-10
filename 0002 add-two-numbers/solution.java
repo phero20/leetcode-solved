f2dc22dd178d2cdb1c36e623a60ce3d8cf895fb1
Feroz <mdferozahmed27156@gmail.com>
2026-04-10 15:27:15 +0000

    Solved Add Two Numbers

--- 0002 add-two-numbers/solution.java
+++ 0002 add-two-numbers/solution.java
@@ -1,32 +1,31 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Definition for singly-linked list.
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode temp = dummy;
-        int carry=0;
-        while(l1!=null || l2!=null || carry!=0) {
-            int sum=carry;
-            if(l1!=null) {
-                sum+=l1.val;
-                l1=l1.next;
+        ListNode curr = dummy;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-            if(l2!=null) {
-                sum+=l2.val;
-                l2=l2.next;
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
-            carry=sum/10;
-            temp.next = new ListNode(sum%10);
-            temp=temp.next;
+            
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
         }
+        
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        
         return dummy.next;
     }
 }
