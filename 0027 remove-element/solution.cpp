0aeb1149ea1672bee1c8e888c913975d7ec20b0c
Feroz <mdferozahmed27156@gmail.com>
2026-04-14 06:44:36 +0000

    Solved Remove Element

---
+++ 0027 remove-element/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int l = 0;
+        for(int r=0;r<nums.size();r++) {
+            if(nums[r]!=val) {
+                nums[l++] = nums[r];
+            }
+        }
+        return l;
+    }
+};
