1e301bf0be6e278041bdc3765b8e8a53327cb658
Feroz <mdferozahmed27156@gmail.com>
2026-04-15 12:53:53 +0000

    Solved Valid Anagram

--- 0242 valid-anagram/solution.rs
+++ 0242 valid-anagram/solution.rs
@@ -1,14 +1,20 @@
 impl Solution {
     pub fn is_anagram(s: String, t: String) -> bool {
-    let mut chars1: Vec<char> = s.chars().collect();
-    let mut chars2: Vec<char> = t.chars().collect();
-
-    chars1.sort_unstable();
-    chars2.sort_unstable();
-
-    let sorted1: String = chars1.into_iter().collect();
-    let sorted2: String = chars2.into_iter().collect();
-
-    return sorted1==sorted2 
+        if s.len() != t.len() {
+            return false;
+        }
+        let mut c = [0; 26];
+        let s1 = s.as_bytes();
+        let s2 = t.as_bytes();
+        for i in 0..s.len() {
+            c[(s1[i]-b'a') as usize]+=1;
+            c[(s2[i]-b'a') as usize]-=1;
+        }
+        for i in 0..c.len() {
+            if c[i]!=0 {
+                return false;
+            }
+        }
+        return true;
     }
 }
