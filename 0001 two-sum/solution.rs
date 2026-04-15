2a16b4beed006bf166933168d95b072e846c25f6
Feroz <mdferozahmed27156@gmail.com>
2026-04-15 13:01:14 +0000

    Solved Two Sum

--- 0001 two-sum/solution.rs
+++ 0001 two-sum/solution.rs
@@ -1,19 +1,14 @@
 use std::collections::HashMap;
-
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = HashMap::new();
-
-        for (i, &num) in nums.iter().enumerate() {
-            let diff = target - num;
-
-            if let Some(&index) = map.get(&diff) {
-                return vec![index as i32, i as i32];
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        for i in 0..nums.len() {
+            let diff = target-nums[i];
+            if let Some(&idx) = map.get(&diff) {
+                return vec![idx as i32,i as i32]
             }
-
-            map.insert(num, i);
+            map.insert(nums[i],i as i32);
         }
-
-        vec![]
+        return vec![]
     }
 }
