a52f56f13e9aea9b65292624d5d0204fd5820088
Feroz <mdferozahmed27156@gmail.com>
2026-04-21 07:16:37 +0000

    Solved Maximum Ascending Subarray Sum

---
+++ 1800 maximum-ascending-subarray-sum/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int sum = nums[0],m = nums[0];
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i] > nums[i-1]) sum+=nums[i];
+            else {
+                m = max(m,sum);
+                sum = nums[i];
+            }
+        }
+        return max(m,sum);
+    }
+};
