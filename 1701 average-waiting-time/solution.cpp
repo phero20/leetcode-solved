9a6925ee76feb74a2ddbdf537be4b25eaa7fe769
Feroz <mdferozahmed27156@gmail.com>
2026-04-29 04:47:34 +0000

    Solved Average Waiting Time

---
+++ 1701 average-waiting-time/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        int sum = 0;
+        double ans = 0;
+        for(vector<int> c:customers) {
+            if(c[0] > sum) {
+                ans+=c[1];
+                sum=c[1]+c[0];
+            } else {
+                sum+=c[1];
+                ans+=sum-c[0];
+            }
+        }
+        return ans/customers.size();
+    }
+};
