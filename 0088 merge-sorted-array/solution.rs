93327d33f9fcd523b2124bf9f02cb9b290259d3b
Feroz <mdferozahmed27156@gmail.com>
2026-05-06 03:44:40 +0000

    Solved Merge Sorted Array

--- 0088 merge-sorted-array/solution.rs
+++ 0088 merge-sorted-array/solution.rs
@@ -1,21 +1,17 @@
 impl Solution {
-    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        let mut i = m as isize - 1;
-        let mut j = n as isize - 1;
-
-        for k in (0..(m+n) as usize).rev() {
-            if j < 0 {
-                nums1[k] = nums1[i as usize];
-                i -= 1;
-            } else if i < 0 {
-                nums1[k] = nums2[j as usize];
-                j -= 1;
-            } else if nums1[i as usize] > nums2[j as usize] {
-                nums1[k] = nums1[i as usize];
-                i -= 1;
+    pub fn merge(nums1: &mut Vec<i32>,mut m: i32, nums2: &mut Vec<i32>,mut n: i32) {
+        let mut i = m+n-1;
+        m-=1;
+        n-=1;
+        while n>=0 {
+            if m>=0 && nums1[m as usize] > nums2[n as usize] {
+                nums1[i as usize] = nums1[m as usize];
+                m-=1;
+                i-=1;
             } else {
-                nums1[k] = nums2[j as usize];
-                j -= 1;
+                nums1[i as usize] = nums2[n as usize];
+                n-=1;
+                i-=1;
             }
         }
     }
