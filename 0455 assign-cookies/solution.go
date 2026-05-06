ddc409339165c24d322469d1fba9864f42661189
Feroz <mdferozahmed27156@gmail.com>
2026-05-06 06:15:25 +0000

    Solved Assign Cookies

---
+++ 0455 assign-cookies/solution.go
@@ -0,0 +1,14 @@
+func findContentChildren(g []int, s []int) int {
+    sort.Ints(g);
+    sort.Ints(s);
+    i,j:=0,0
+    for i<len(g) && j<len(s) {
+        if s[j] >= g[i] {
+            i++
+            j++
+        } else {
+            j++
+        }
+    }
+    return i
+}
