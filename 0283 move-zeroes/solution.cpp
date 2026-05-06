7c6d23bf925fcd5c3e2158e13b14d272168631bf
Feroz <mdferozahmed27156@gmail.com>
2026-05-06 04:37:54 +0000

    Solved Move Zeroes

---
+++ 0283 move-zeroes/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]!=0) {
+                int temp = nums[l];
+                nums[l++] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+};
