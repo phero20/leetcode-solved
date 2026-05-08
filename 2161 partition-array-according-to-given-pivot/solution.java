89b47c25437707472e4a795610edfb0605081d00
Feroz <mdferozahmed27156@gmail.com>
2026-05-08 08:13:56 +0000

    Solved Partition Array According to Given Pivot

---
+++ 2161 partition-array-according-to-given-pivot/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int p1 = 0, p2 = 0, p3 = 0;
+        int[] ans = new int[nums.length];
+        for (int i : nums) {
+            if (i < pivot)
+                p2++;
+            else if (i == pivot)
+                p3++;
+        }
+        p3 += p2;
+        for (int i : nums) {
+            if (i < pivot)
+                ans[p1++] = i;
+            else if (i == pivot)
+                ans[p2++] = i;
+            else
+                ans[p3++] = i;
+        }
+        return ans;
+    }
+}
