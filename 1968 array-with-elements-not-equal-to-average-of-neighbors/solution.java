aba05a82c2223a8452891abd240851f3ebb0a141
Feroz <mdferozahmed27156@gmail.com>
2026-05-09 04:57:39 +0000

    Solved Array With Elements Not Equal to Average of Neighbors

---
+++ 1968 array-with-elements-not-equal-to-average-of-neighbors/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        for(int i=1;i<nums.length-1;i++) {
+            if(2*nums[i] == nums[i-1] + nums[i+1]) {
+                int temp = nums[i+1];
+                nums[i+1] = nums[i];
+                nums[i] = temp;
+                if(i>1) i-=2;
+            }
+        }
+        return nums;
+    }
+}
