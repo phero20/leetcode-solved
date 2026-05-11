3882bc874ae38103a4c65fe0e04e30384c35a5f0
Feroz <mdferozahmed27156@gmail.com>
2026-05-11 06:36:26 +0000

    Solved K-th Symbol in Grammar

---
+++ 0779 k-th-symbol-in-grammar/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if (n == 1) return  0;
+        int ans = kthGrammar(n-1,(k+1)/2);
+        if(ans == 0) return ( (k & 1) == 1 ? 0 : 1);
+        return ( (k & 1) == 1 ? 1 : 0);
+    }
+}
