b1c75bbedc60f3d2eeb29eeb14a9b17738b50053
Feroz <mdferozahmed27156@gmail.com>
2026-05-12 04:35:06 +0000

    Solved Trapping Rain Water

---
+++ 0042 trapping-rain-water/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l=0,rm=0,lm=0,r=height.size()-1,ans=0;
+        while(l<r) {
+            if(height[l] < height[r]) {
+                if(height[l] >= lm) {
+                    lm=height[l];
+                } else {
+                    ans+=lm-height[l];
+                }
+                l++;
+            } else {
+                if(height[r] >= rm) {
+                    rm = height[r];
+                } else {
+                    ans+=rm-height[r];
+                }
+                r--;
+            }
+        }
+        return ans;
+    }
+};
