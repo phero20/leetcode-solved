b3ea2b2bc6a71d0fcb08efc7fcb3793a8582c813
Feroz <mdferozahmed27156@gmail.com>
2026-05-12 06:14:10 +0000

    Solved Final Prices With a Special Discount in a Shop

--- 1475 final-prices-with-a-special-discount-in-a-shop/solution.java
+++ 1475 final-prices-with-a-special-discount-in-a-shop/solution.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-         Deque<Integer> st = new ArrayDeque<>();
-        int[] res = prices.clone();
-        for(int i=0;i<prices.length;i++) {
-            while(!st.isEmpty() && prices[st.peek()]>=prices[i]) {
-                res[st.pop()]-=prices[i];
+        int[] st = new int[prices.length];
+        int top = 0,idx=0;
+        int[] ans = new int[prices.length];
+        for(int i=1;i<prices.length;i++) {
+            while(top!=-1 && prices[st[top]] >= prices[i]) {
+                ans[st[top]] = prices[st[top--]] - prices[i]; 
             }
-            st.push(i);
+            st[++top] = i;
         }
-        return res;
+        for(int i=0;i<=top;i++) ans[st[i]] = prices[st[i]];
+        return ans;
     }
 }
