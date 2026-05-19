ddef681aa3df494d951e6579d5d925f66ede7d65
Feroz <mdferozahmed27156@gmail.com>
2026-05-19 06:52:40 +0000

    Solved Asteroid Collision

---
+++ 0735 asteroid-collision/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+       vector<int> st;
+        for (int a : asteroids) {
+            bool alive = true;
+            while (alive && !st.empty() && st.back() > 0 && a < 0) {
+                if (st.back() < -a) {
+                    st.pop_back(); 
+                } else if (st.back() == -a) {
+                    st.pop_back(); 
+                    alive = false;
+                } else {
+                    alive = false; 
+                }
+            }
+            if (alive) st.push_back(a);
+        }
+        return st;
+    }
+}
+;
