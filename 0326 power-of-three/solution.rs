138b55066cdc5a3c4d82ef822ba85784bb5f6587
Feroz <mdferozahmed27156@gmail.com>
2026-05-21 06:35:39 +0000

    Solved Power of Three

---
+++ 0326 power-of-three/solution.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn is_power_of_three(n: i32) -> bool {
+        if n==1 {
+            return true
+        }
+        if n==0 || n%3!=0 {
+            return false
+        }
+        return Self::is_power_of_three(n/3)
+    }
+}
