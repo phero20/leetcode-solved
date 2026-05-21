6f4a0f17dfc89c481a32da40e4ff7fac4275a90e
Feroz <mdferozahmed27156@gmail.com>
2026-05-21 06:31:57 +0000

    Solved Power of Two

--- 0231 power-of-two/solution.go
+++ 0231 power-of-two/solution.go
@@ -1,4 +1,9 @@
 func isPowerOfTwo(n int) bool {
-    if(n<=0){ return false}
-    return (n&(n-1))==0
+        if n==1 {
+            return true
+        }
+        if n==0 || n%2!=0 {
+            return false
+        }
+        return isPowerOfTwo(n/2);
 }
