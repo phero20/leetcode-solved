1fb1b8e6eabfb1e6b257a06a1beb9bf1fe777d69
Feroz <mdferozahmed27156@gmail.com>
2026-05-22 05:24:20 +0000

    Solved Check If It Is a Straight Line

---
+++ 1232 check-if-it-is-a-straight-line/solution.go
@@ -0,0 +1,13 @@
+func checkStraightLine(coordinates [][]int) bool {
+        x0,y0:= coordinates[0][0], coordinates[0][1]
+        x1,y1:= coordinates[1][0], coordinates[1][1]
+        dx,dy:= x1-x0,y1-y0
+
+        for i:= 2; i < len(coordinates); i++ {
+            xi,yi:= coordinates[i][0], coordinates[i][1]
+            if ((yi - y0) * dx != (xi - x0) * dy) {
+                return false
+            }
+        }
+        return true
+}
