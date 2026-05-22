7f8ddfb768a4aa99a84a79efd34628e6d3a34721
Feroz <mdferozahmed27156@gmail.com>
2026-05-22 05:22:09 +0000

    Solved Check If It Is a Straight Line

---
+++ 1232 check-if-it-is-a-straight-line/solution.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool CheckStraightLine(int[][] coordinates) {
+        int x0 = coordinates[0][0], y0 = coordinates[0][1];
+        int x1 = coordinates[1][0], y1 = coordinates[1][1];
+        int dx = x1 - x0, dy = y1 - y0;
+
+        for (int i = 2; i < coordinates.Length; i++) {
+            int xi = coordinates[i][0], yi = coordinates[i][1];
+            if ((yi - y0) * dx != (xi - x0) * dy) return false;
+        }
+        return true;
+    }
+}
