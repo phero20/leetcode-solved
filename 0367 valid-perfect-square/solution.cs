39a1e7fc080058d2a7dd5613d2191d728b8fa2e9
Feroz <mdferozahmed27156@gmail.com>
2026-05-24 07:06:08 +0000

    Solved Valid Perfect Square

---
+++ 0367 valid-perfect-square/solution.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsPerfectSquare(int num) {
+        if (num < 2) return true;
+        long l = 1, r = num / 2;
+        while (l <= r) {
+            long m = l + (r - l) / 2;
+            long v = m * m;
+            if (v == num) return true;
+            else if (v < num) l = m + 1;
+            else r = m - 1;
+        }
+        return false;
+    }
+}
