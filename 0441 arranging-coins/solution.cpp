9c375cfb167cc0fbae53ac639a2985957f9db7be
Feroz <mdferozahmed27156@gmail.com>
2026-05-24 06:53:17 +0000

    Solved Arranging Coins

---
+++ 0441 arranging-coins/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long l=0,r=n;
+        while(l<=r) {
+            long long m = l+(r-l)/2;
+            long long v=(m*(m+1))/2;
+            if(v==n) return (int)m;
+            if(v<n) l=m+1;
+            else r=m-1;
+        }
+        return (int)r;
+    }
+};
