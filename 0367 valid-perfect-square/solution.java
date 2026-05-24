a3b02f90bfbf63fff52301615dedd16182aa50e9
Feroz <mdferozahmed27156@gmail.com>
2026-05-24 07:05:36 +0000

    Solved Valid Perfect Square

---
+++ 0367 valid-perfect-square/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num < 2) return true;
+        long l = 1, r = num / 2;
+        while (l <= r) {
+            long m = l + (r - l) / 2;
+            long v = m * m;
+            if (v == num) return true;
+            else if (v < num) l = m + 1;
+            else r = m - 1;
+        }
+        return false;
+    }
+}
+
