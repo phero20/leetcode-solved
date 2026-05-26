f17e065288c50523842af00b4f47748fb8322312
Feroz <mdferozahmed27156@gmail.com>
2026-05-26 05:02:18 +0000

    Solved Capacity To Ship Packages Within D Days

--- 1011 capacity-to-ship-packages-within-d-days/solution.cs
+++ 1011 capacity-to-ship-packages-within-d-days/solution.cs
@@ -1,31 +1,31 @@
 public class Solution {
     public int ShipWithinDays(int[] weights, int days) {
-        int l=0,r=0;
-        foreach(var i in weights) {
-            l=Math.Max(l,i);
-            r+=i;
+        int r = 0, l = 0;
+        foreach(int i in weights) {
+            l = Math.Max(l, i);
+            r += i;
         }
-        int res=r;
-        while(l<=r) {
-            int mid = l+(r-l)/2;
-            if(isPos(weights,mid,days)) {
-                res=mid;
-                r=mid-1;
+        int res = r;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (pos(weights, mid, days)) {
+                res = mid;
+                r = mid - 1;
             } else {
-                l=mid+1;
+                l = mid + 1;
             }
         }
         return res;
     }
-    private bool isPos(int[] nums,int mid,int days) {
-        int d=1,cr=0;
-        foreach(var i in nums) {
-            if(cr+i > mid) {
-                d++;
-                cr=0;
+    private bool pos(int[] nums, int n, int h) {
+        int sum = 0, c = 0;
+        foreach(int i in nums) {
+            if (sum + i > n) {
+                c++;
+                sum = 0;
             }
-            cr+=i;
+            sum += i;
         }
-        return d<=days;
+        return c+1 <= h;
     }
 }
