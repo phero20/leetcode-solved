2c83d167935db8c343d985de95a9aacbd0824d5d
Feroz <mdferozahmed27156@gmail.com>
2026-06-23 11:31:34 +0530

    Solved Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold

---
+++ 1343 number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int sum = 0, ans = 0, t=k*threshold;
+        for(int i=0;i<k;i++) {
+            sum+=arr[i];
+        }
+        if(sum>=t) {
+            ans++;
+        }
+        for(int i=k;i<arr.size();i++) {
+            sum+=arr[i];
+            sum-=arr[i-k];
+            if(sum >= t) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
