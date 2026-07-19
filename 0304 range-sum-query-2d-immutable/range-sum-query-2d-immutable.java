54da053d8b7ad2519576cce3e4bd24c194f9c4d5
Feroz <mdferozahmed27156@gmail.com>
2026-07-19 12:57:43 +0530

    Solved Range Sum Query 2D - Immutable

---
+++ 0304 range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -0,0 +1,25 @@
+class NumMatrix {
+    int[][] nums;
+    public NumMatrix(int[][] matrix) {
+        int n = matrix.length,m = matrix[0].length;
+        nums = new int[n+1][m+1];
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                nums[i][j] = nums[i][j-1] + 
+                nums[i-1][j] + 
+                matrix[i-1][j-1] - 
+                nums[i-1][j-1];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return nums[row2+1][col2+1] - nums[row2+1][col1] - nums[row1][col2+1] + nums[row1][col1];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
