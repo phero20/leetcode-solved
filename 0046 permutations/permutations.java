7097da6a4dd29004307c094473a831ce350735c3
Feroz <mdferozahmed27156@gmail.com>
2026-09-01 18:11:00 +0530

    Solved Permutations

---
+++ 0046 permutations/permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    List<List<Integer>> ans;
+    public List<List<Integer>> permute(int[] nums) {
+        ans = new ArrayList<>();
+        bt(nums,0,new ArrayList<>(),new boolean[nums.length+1]);
+        return ans;
+    }
+
+    private void bt(int[] nums,int i,List<Integer> arr,boolean[] u) {
+        if(arr.size()==nums.length) {
+            ans.add(new ArrayList<>(arr));
+            return;
+        }
+        if(i==nums.length) return;
+        if(!u[i]) {
+            arr.add(nums[i]);
+            u[i] = true;
+            bt(nums,0,arr,u);
+            arr.remove(arr.size()-1);
+            u[i] = false;
+        }
+        bt(nums,i+1,arr,u);
+    }
+}
