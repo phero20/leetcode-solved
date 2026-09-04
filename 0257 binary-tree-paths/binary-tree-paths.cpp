e3ca589cb5bbecf766131026538b1ab7eb324d37
Feroz <mdferozahmed27156@gmail.com>
2026-09-04 18:47:49 +0530

    Solved Binary Tree Paths

---
+++ 0257 binary-tree-paths/binary-tree-paths.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        dfs(root,"",ans);
+        return ans;
+    }
+
+    void dfs(TreeNode* root, string sb, vector<string>& ans) {
+        sb+=to_string(root->val);
+        if(!root->left && !root->right) {
+            ans.push_back(sb);
+        }
+        if(root->left) dfs(root->left,sb+"->",ans);
+        if(root->right) dfs(root->right,sb+"->",ans);
+    }
+};
