15294d9a6b5dd511a45f2d8333b71a50d854d303
Feroz <mdferozahmed27156@gmail.com>
2026-09-09 21:03:49 +0530

    Solved Car Pooling

---
+++ 1094 car-pooling/car-pooling.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] ct = new int[10001];
+        for(int[] t:trips) {
+            ct[t[1]]+=t[0];
+            ct[t[2]]-=t[0];
+        }
+        int ans = 0;
+        for(int v:ct) {
+            ans+=v;
+            if(ans > capacity) return false;
+        }
+        return true;
+    }
+}
