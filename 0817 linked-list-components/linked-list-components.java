17d31bf52bf206556f0f001880c68aff4d6f9990
Feroz <mdferozahmed27156@gmail.com>
2026-09-13 19:56:36 +0530

    Solved Linked List Components

---
+++ 0817 linked-list-components/linked-list-components.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums) set.add(i);
+        int ans = 0;
+        while(head != null) {
+            if(set.contains(head.val) && 
+              (head.next == null || !set.contains(head.next.val))
+            ) ans++;
+            head = head.next;
+        }
+        return ans;
+    }
+}
